b8184063958d06004b40410dcfb505cebafca5be
ambleside138 <ambleside.138@gmail.com>
2015-12-06 23:33:06 +0900

    ListBox Test

--- ControlsShowcase/Models/Model.cs
+++ ControlsShowcase/Models/Model.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 
 using Livet;
+using System.Collections.ObjectModel;
 
 namespace ControlsShowcase.Models
 {
     public class Model : NotificationObject
     {
-        /*
-         * NotificationObjectはプロパティ変更通知の仕組みを実装したオブジェクトです。
-         */
-    }
+
+
+    } 
 }
---
+++ ControlsShowcase/Models/Staff.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Livet;
+
+namespace ControlsShowcase.Models
+{
+    public class Staff : NotificationObject
+    {
+
+        #region Id変更通知プロパティ
+        private int _Id;
+
+        public int Id
+        {
+            get
+            { return _Id; }
+            set
+            { 
+                if (_Id == value)
+                    return;
+                _Id = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Name変更通知プロパティ
+        private string _Name;
+
+        public string Name
+        {
+            get
+            { return _Name; }
+            set
+            { 
+                if (_Name == value)
+                    return;
+                _Name = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Memo変更通知プロパティ
+        private string _Memo;
+
+        public string Memo
+        {
+            get
+            { return _Memo; }
+            set
+            { 
+                if (_Memo == value)
+                    return;
+                _Memo = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+    }
+}
