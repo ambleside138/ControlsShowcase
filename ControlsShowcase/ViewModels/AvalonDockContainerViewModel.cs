4c360402d840dfd5d7e7df216cd1d16e5e0f835d
ambleside138 <ambleside.138@gmail.com>
2015-12-12 18:04:08 +0900

    AddAvalonControl

---
+++ ControlsShowcase/ViewModels/AvalonDockContainerViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using ControlsShowcase.Models;
+
+namespace ControlsShowcase.ViewModels
+{
+    public class AvalonDockContainerViewModel : ViewModel
+    {
+
+        #region ShowChildWindowCommand
+        private ViewModelCommand _ShowChildWindowCommand;
+
+        public ViewModelCommand ShowChildWindowCommand
+        {
+            get
+            {
+                if (_ShowChildWindowCommand == null)
+                {
+                    _ShowChildWindowCommand = new ViewModelCommand(ShowChildWindow);
+                }
+                return _ShowChildWindowCommand;
+            }
+        }
+
+        public void ShowChildWindow()
+        {
+            Messenger.Raise(new TransitionMessage("ShowCommand"));
+        }
+        #endregion
+
+    }
+}
