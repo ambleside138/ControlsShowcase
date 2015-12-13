433732a304dc207c7d46aef52866db8e52aca94a
ambleside138 <ambleside.138@gmail.com>
2015-12-13 22:55:33 +0900

    Implement ListBoxFilter

---
+++ ControlsShowcase/ViewModels/CollectionSourceViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using ControlsShowcase.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+using Livet.EventListeners.WeakEvents;
+
+namespace ControlsShowcase.ViewModels
+{
+    public class CollectionSourceViewModel<T> : ViewModel
+    {
+
+        #region CollectionSource変更通知プロパティ
+        private ObservableCollection<T> _CollectionSource;
+
+        public ObservableCollection<T> CollectionSource
+        {
+            get
+            { return _CollectionSource; }
+
+            private set
+            { 
+                if (_CollectionSource == value)
+                    return;
+                _CollectionSource = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        public ICollectionView View { get; private set; }
+
+
+        public CollectionSourceViewModel(IEnumerable<T> source, Func<T,bool> filter)
+        {
+            CollectionSource = new ObservableCollection<T>(source);
+
+            // Viewの作成
+            View = CollectionViewSource.GetDefaultView(CollectionSource);
+            View.Filter = x => filter((T)x);
+        }
+
+        public void UpdateFilter()
+        {
+            View.Refresh();
+        }
+
+        public T SelectedItem
+        {
+            get { return (T)View.CurrentItem; }
+        }
+
+
+    }
+}
--- ControlsShowcase/ViewModels/FilterTabViewViewModel.cs
+++ ControlsShowcase/ViewModels/FilterTabViewViewModel.cs
@@ -17,6 +17,110 @@ namespace ControlsShowcase.ViewModels
 {
     public class FilterTabViewViewModel : ViewModel
     {
-        
+
+        #region StaffCollection変更通知プロパティ
+        private CollectionSourceViewModel<Staff> _StaffCollection;
+
+        public CollectionSourceViewModel<Staff> StaffCollection
+        {
+            get
+            { return _StaffCollection; }
+            set
+            { 
+                if (_StaffCollection == value)
+                    return;
+                _StaffCollection = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        #region NameFilter変更通知プロパティ
+        private string _NameFilter;
+
+        public string NameFilter
+        {
+            get
+            { return _NameFilter; }
+            set
+            { 
+                if (_NameFilter == value)
+                    return;
+                _NameFilter = value;
+                RaisePropertyChanged();
+                StaffCollection.UpdateFilter();
+            }
+        }
+        #endregion
+
+        #region SelectedName変更通知プロパティ
+        private string _SelectedName;
+
+        public string SelectedName
+        {
+            get
+            { return _SelectedName; }
+            set
+            { 
+                if (_SelectedName == value)
+                    return;
+                _SelectedName = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        public FilterTabViewViewModel()
+        {
+            StaffCollection = new CollectionSourceViewModel<Staff>(Enumerable.Range(1, 999).Select(i => new Staff { Id = i, Name = i + "TEST" }), s => Filter(s));
+            StaffCollection.View.CurrentChanged += View_CurrentChanged;
+        }
+
+        private void View_CurrentChanged(object sender, EventArgs e)
+        {
+            SelectCommand.RaiseCanExecuteChanged();
+        }
+
+        private bool Filter(Staff staff)
+        {
+            if(String.IsNullOrWhiteSpace(NameFilter))
+            {
+                return true;
+            }
+
+            return staff.Name.StartsWith(NameFilter);
+        }
+
+
+
+        #region SelectCommand
+        private ViewModelCommand _SelectCommand;
+
+        public ViewModelCommand SelectCommand
+        {
+            get
+            {
+                if (_SelectCommand == null)
+                {
+                    _SelectCommand = new ViewModelCommand(Select, CanSelect);
+                }
+                return _SelectCommand;
+            }
+        }
+
+        public bool CanSelect()
+        {
+            return StaffCollection.SelectedItem != null;
+        }
+
+        public void Select()
+        {
+            SelectedName = StaffCollection.SelectedItem.Name;
+        }
+        #endregion
+
+
     }
 }
