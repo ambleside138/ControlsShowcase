3abf41beedbceef746f3c812ce07e4336dd31582
ambleside138 <ambleside.138@gmail.com>
2015-12-13 21:55:36 +0900

    Add FilterTab

---
+++ ControlsShowcase/ViewModels/FilterTabViewViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using ControlsShowcase.Models;
+
+namespace ControlsShowcase.ViewModels
+{
+    public class FilterTabViewViewModel : ViewModel
+    {
+        
+    }
+}
