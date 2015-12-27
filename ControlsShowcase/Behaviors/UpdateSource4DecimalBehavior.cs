51367c7add1f2886df533a4e462d121accad4cfa
ambleside138 <ambleside.138@gmail.com>
2015-12-27 16:00:29 +0900

    Add Behavior for DecimalBind

---
+++ ControlsShowcase/Behaviors/UpdateSource4DecimalBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace ControlsShowcase.Behaviors
+{
+    /// <summary>
+    /// 小数をテキストボックスにバインドするときに利用するBehavior
+    /// </summary>
+    public class UpdateSource4DecimalBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.TextChanged += AssociatedObject_TextChanged;
+        }
+
+
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.TextChanged -= AssociatedObject_TextChanged;
+        }
+
+
+        private void AssociatedObject_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // 末尾が「.」のときはソースを更新しない
+            if (!string.IsNullOrEmpty(AssociatedObject.Text) && AssociatedObject.Text.EndsWith("."))
+            {
+                return;
+            }
+
+            // 更新
+            var be = AssociatedObject.GetBindingExpression(TextBox.TextProperty);
+            be.UpdateSource();
+            
+        }
+    }
+}
---
+++ ControlsShowcase/ViewModels/StringFormatViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using ControlsShowcase.Models;
+
+namespace ControlsShowcase.ViewModels
+{
+    public class StringFormatViewModel : ViewModel
+    {
+
+        #region Sample1変更通知プロパティ
+        private decimal _Sample1;
+
+        public decimal Sample1
+        {
+            get
+            { return _Sample1; }
+            set
+            { 
+                if (_Sample1 == value)
+                    return;
+                _Sample1 = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Sample2変更通知プロパティ
+        private decimal _Sample2;
+
+        public decimal Sample2
+        {
+            get
+            { return _Sample2; }
+            set
+            { 
+                if (_Sample2 == value)
+                    return;
+                _Sample2 = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        public StringFormatViewModel()
+        {
+            Sample1 = 1.00M;/// 1.235556M;
+            Sample2 = 3.45M;
+        }
+    }
+}
