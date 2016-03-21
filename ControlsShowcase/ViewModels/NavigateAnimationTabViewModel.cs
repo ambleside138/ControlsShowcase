74906ecee3ec99c6ce477e2f81b5d57f398b6c97
ambleside138 <ambleside.138@gmail.com>
2016-03-21 23:26:47 +0900

    add animationtab

---
+++ ControlsShowcase/ViewModels/NavigateAnimationTabViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using ControlsShowcase.Models;
+
+namespace ControlsShowcase.ViewModels
+{
+    public class NavigateAnimationTabViewModel : ViewModel
+    {
+        private readonly NavigationPageViewModel _Page1;
+        private readonly NavigationPageViewModel _Page2;
+
+        #region Content変更通知プロパティ
+        private NavigationPageViewModel _Content;
+
+        public NavigationPageViewModel Content
+        {
+            get
+            { return _Content; }
+            set
+            { 
+                if (_Content == value)
+                    return;
+                _Content = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Page1Command
+        private ViewModelCommand _Page1Command;
+
+        public ViewModelCommand Page1Command
+        {
+            get
+            {
+                if (_Page1Command == null)
+                {
+                    _Page1Command = new ViewModelCommand(Page1);
+                }
+                return _Page1Command;
+            }
+        }
+
+        public void Page1()
+        {
+            Content = _Page1;
+            _Page1.SendAnimationTriggerMessege();
+        }
+        #endregion
+
+        #region Page2Command
+        private ViewModelCommand _Page2Command;
+
+        public ViewModelCommand Page2Command
+        {
+            get
+            {
+                if (_Page2Command == null)
+                {
+                    _Page2Command = new ViewModelCommand(Page2);
+                }
+                return _Page2Command;
+            }
+        }
+
+        public void Page2()
+        {
+            Content = _Page2;
+            _Page2.SendAnimationTriggerMessege();
+        }
+        #endregion
+
+        public NavigateAnimationTabViewModel()
+        {
+            _Page1 = new NavigationPageViewModel("This is Page1");
+            _Page1.AddStaffs("zimu", 20);
+
+            _Page2 = new NavigationPageViewModel("### PAGE 2 ###");
+            _Page2.AddStaffs("dev", 5);
+
+            Content = _Page1;
+        }
+
+        public void Initialize()
+        {
+        }
+    }
+}
---
+++ ControlsShowcase/ViewModels/NavigationPageViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Livet;
+using Livet.Commands;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.EventListeners;
+using Livet.Messaging.Windows;
+
+using ControlsShowcase.Models;
+
+namespace ControlsShowcase.ViewModels
+{
+    public class NavigationPageViewModel : ViewModel
+    {
+        private readonly Model _Model = new Model();
+
+        #region PageTitle変更通知プロパティ
+        private string _PageTitle;
+
+        public string PageTitle
+        {
+            get
+            { return _PageTitle; }
+            set
+            { 
+                if (_PageTitle == value)
+                    return;
+                _PageTitle = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region ButtonText変更通知プロパティ
+        private string _ButtonText;
+
+        public string ButtonText
+        {
+            get
+            { return _ButtonText; }
+            set
+            { 
+                if (_ButtonText == value)
+                    return;
+                _ButtonText = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        #region Staffs
+        private ReadOnlyDispatcherCollection<StaffViewModel> _Staffs;
+
+        public ReadOnlyDispatcherCollection<StaffViewModel> Staffs
+        {
+            get
+            {
+                if (_Staffs == null)
+                {
+                    _Staffs = ViewModelHelper.CreateReadOnlyDispatcherCollection(
+                        _Model.Staffs,
+                        m => new StaffViewModel(m),
+                        DispatcherHelper.UIDispatcher);
+                }
+
+                return _Staffs;
+            }
+
+        } 
+        #endregion
+
+        public NavigationPageViewModel(string title)
+        {
+            PageTitle = title;
+        }
+
+
+        public void AddStaffs(string deartment, int count)
+        {
+            foreach (var oStaff in Enumerable.Range(1, count).Select(i => new Staff { Name = "スタッフ" + i }))
+            {
+                oStaff.Department = deartment;
+                _Model.Staffs.Add(oStaff);
+            }
+        }
+
+        public void SendAnimationTriggerMessege()
+        {
+            Messenger.Raise(new InteractionMessage("StartSlideInAnimationKey"));
+        }
+
+        public void SendSlideOutAnimationTriggerMessege()
+        {
+            Messenger.Raise(new InteractionMessage("StartSlideOutAnimationKey"));
+        }
+    }
+}
